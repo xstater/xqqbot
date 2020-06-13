2bed776a54a31bb335fd8eeab5062e03aa07d187
xstater <a166224@163.com>
2020-06-14 05:43:19 +0800

    added test

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Web.Scotty
+import Data.Aeson
+import Data.Aeson.Cursor
+
+-- main :: IO ()
+-- main = scotty 23312 $ do
+--     post "/" $ do
+--         bodyData <- body
+--         liftAndCatchIO $ print bodyData
 
 main :: IO ()
-main = someFunc
+main = do
+    let jsn = decode "{\"info\":{\"name\":\"fuck\",\"age\":123}}" :: Maybe Value
+    print $ jsn /@ "info" /@ "name"
+    print $ jsn /@ "info" /@ "age"
+    print $ jsn /@ "nmsl" /@ "shit"
+    print $ jsn /@ "info" /@ "fuck"
